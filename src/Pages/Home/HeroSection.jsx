6aa4729448d3b5d59a612854d6aee8134f1d8e9a
Nadir <DragonCode95@outlook.com>
2025-11-21 18:05:45 +0000

    first commit

---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,28 @@
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm Nadir Ahmed Your's new</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Software</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+            A warm welcome to my portfolio website.
+            <br /> 
+          </p>
+        </div>
+
+        <button className="btn btn-primary">
+            
+            Get In Touch</button>
+      </div>
+      
+      <div className="hero--section--img">
+        <img src="./img/techs.png" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
